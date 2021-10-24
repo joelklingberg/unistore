a7e101e32571989a5d1b9f3b0daee536e098c243
Joel Klingberg <joel.klingberg@hotmail.com>
2021-10-24 10:54:54 +0200

    Foreign key mapping now working for order request -> product.

--- src/main/java/com/unistore/order/dto/OrderMapper.java
+++ src/main/java/com/unistore/order/dto/OrderMapper.java
@@ -4,48 +4,38 @@ import com.unistore.order.dto.request.OrderRequest;
 import com.unistore.order.dto.request.OrderRowRequest;
 import com.unistore.order.dto.response.OrderResponse;
 import com.unistore.order.dto.response.OrderRowResponse;
-
 import java.util.List;
-
 import com.unistore.order.Order;
 import com.unistore.order.OrderRow;
+import com.unistore.product.ProductService;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = ProductService.class)
 public interface OrderMapper {
 
-    /* OrderRequest */
-
-    // OrderRequest -> Order
     Order orderRequestToOrder(OrderRequest orderRequest);
 
-    // Order -> OrderRequest
     OrderRequest orderToOrderRequest(Order order);
 
-    // OrderRows -> OrderRowRequests
     List<OrderRowRequest> orderRowsToOrderRowRequests(List<OrderRow> orderRows);
 
-    // OrderRowRequests -> OrderRows
     List<OrderRow> orderRowRequestsToOrderRows(List<OrderRowRequest> orderRowRequests);
 
-    /* OrderResponse */
+    @Mapping(source = "productId", target = "product")
+    OrderRow orderRowRequestToOrderRow(OrderRowRequest orderRowRequest);
 
-    // OrderResponse -> Order
+    @Mapping(source = "product.id", target = "productId")
+    OrderRowResponse orderRowToOrderRowResponse(OrderRow orderRow);
+
     Order orderResponseToOrder(OrderResponse orderResponse);
 
-    // Order -> OrderResponse
     OrderResponse orderToOrderResponse(Order order);
 
     List<OrderResponse> ordersToOrderResponses(List<Order> orders);
 
-    // OrderRows -> OrderRowResponses
-    @Mapping(source = "product.id", target = "productId")
     List<OrderRowResponse> orderRowsToOrderRowResponses(List<OrderRow> orderRows);
 
-    // OrderRowResponses -> OrderRows
-    @Mapping(source = "productId", target = "product.id")
     List<OrderRow> orderRowResponsesToOrderRows(List<OrderRowResponse> orderRowResponses);
-
 
 }
